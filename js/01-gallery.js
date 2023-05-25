9ae63c893146d49f3b4f7158ec0bf01c48ed73bb
Michael-Zhinchyn <124111477+Michael-Zhinchyn@users.noreply.github.com>
2023-05-25 18:33:47 -0500

    markup created update

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,30 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+
+const list = document.querySelector('.gallery')
+
+const markup = galleryItems.map(item => {list.insertAdjacentHTML("beforeend", `<li class="gallery__item">
+<a class="gallery__link" href="${item.original}">
+  <img
+    class="gallery__image"
+    src="${item.preview}"
+    data-source="${item.original}"
+    alt="${item.description}"
+  />
+</a>
+</li>`)})
+
+
+
+
+
+
+   
+
+
+
+
+
+
