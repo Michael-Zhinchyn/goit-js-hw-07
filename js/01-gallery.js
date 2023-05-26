90c9d1d1a1cc6956ab0527293c85804dc03aa5cf
Michael-Zhinchyn <124111477+Michael-Zhinchyn@users.noreply.github.com>
2023-05-25 20:51:15 -0500

    Update 7hw task1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,20 +1,53 @@
+
 import { galleryItems } from './gallery-items.js';
+
 // Change code below this line
 
-// console.log(galleryItems);
 
 const list = document.querySelector('.gallery')
 
-const markup = galleryItems.map(item => {list.insertAdjacentHTML("beforeend", `<li class="gallery__item">
-<a class="gallery__link" href="${item.original}">
-  <img
-    class="gallery__image"
-    src="${item.preview}"
-    data-source="${item.original}"
-    alt="${item.description}"
-  />
-</a>
-</li>`)})
+function createGalleryMarkUp(items) {
+    return items.map((item) => `<li class="gallery__item">
+    <a class="gallery__link" href="${item.original}">
+      <img
+        class="gallery__image"
+        src="${item.preview}"
+        data-source="${item.original}"
+        alt="${item.description}"
+      />
+    </a>
+    </li>`).join("")
+}
+
+const addGallaryMarkUp = createGalleryMarkUp(galleryItems)
+list.insertAdjacentHTML('beforeend', addGallaryMarkUp)
+
+
+list.addEventListener('click', onClick) 
+
+function onClick(evt) {
+    evt.preventDefault()
+    
+    if(evt.target.nodeName !== 'IMG') {
+        return
+    }
+    const source = evt.target.dataset.source
+    const instance = basicLightbox.create(`
+    <img src="${source}" width="800" height="600">
+`)
+instance.show()
+
+
+list.addEventListener('keydown', (evt) => {
+    if(evt.code === 'Escape') {
+        instance.close()
+    }
+})
+}
+
+
+
+
 
 
 
